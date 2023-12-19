3d063d130b0223c833142222eb3ff7626eb89cfd
frafishen <104518723+frafishen@users.noreply.github.com>
2023-12-19 12:53:22 +0800

    feat: add the dev mode

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 
 @app.route('/')
