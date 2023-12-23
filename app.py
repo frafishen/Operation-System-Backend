056dbf54a91bbbf38f9f4389760e8a89cba79e7e
zzronggg <75241917+zzronggg@users.noreply.github.com>
2023-12-23 21:54:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask import (Flask, redirect, render_template, request,
 
 import psycopg2
 import psycopg2.extras
-from utils import calculate_growth_rates
+from utils import (calculate_growth_rates, calculate_customer_lifetime_value, form_pert_chart_tree,
+                    calculate_forcasting_data, calculate_avg_purchase_time)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -16,49 +17,18 @@ PASSWORD = 'mis'
 HOST = 'localhost'
 PORT = 8080
 
-
-@app.route('/')
-def index():
-   print('Request for index page received')
-   print("test Hello")
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
 def get_db_connection():
     conn = psycopg2.connect(database=DATABASE_NAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
     return conn
+
 
 @app.errorhandler(500)
 def handle_500(error):
     app.logger.error(f"500 error: {error}")  # Log the error
     return jsonify({"status": "failed", "message": "Internal server error"}), 500
 
-@app.route('/client', methods=['GET'])
-def get_client():
-    conn = get_db_connection()
-    query = 'SELECT * FROM client'
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        cursor.execute(query)
-        data = cursor.fetchall()
-    result = [dict(row) for row in data]
-    return jsonify(result)
 
-@app.route('/client/<id>', methods=['GET'])
+@app.route('/client/<id>/sgr', methods=['GET'])
 def get_company_sales(id):
     conn = get_db_connection()
     query = '''SELECT s.sales_date, SUM(p.price * s.saled_quantity) AS total_sales
@@ -66,149 +36,63 @@ def get_company_sales(id):
             JOIN product p ON s.product_id = p.product_id
             GROUP BY s.sales_date
             ORDER BY s.sales_date;'''
-    
+
     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
         cursor.execute(query)
         data = cursor.fetchall()
     result = [dict(row) for row in data]
     growth_rates = calculate_growth_rates(result)[-6:]
     result = result[-6:]
-    
+
     return jsonify({'sales revenue': result, 'growth rates': growth_rates})
 
-
-# inventory
-@app.route('/inventory', methods=['GET'])
-def get_inventory():
+#Client
+@app.route('/client/<id>/clv', methods=['GET'])
+def get_client_clv(client_id):
     conn = get_db_connection()
-    query = '''SELECT p.product_id, p.product_name, p.quantity, rp.required_pid AS required_product_id
-               FROM product p
-               INNER JOIN required_product rp ON p.product_id = rp.produced_pid;'''
-    
+    query = f'SELECT c.clv, c.avg_monthly_spend, c.avg_lifespan ' \
+            f'FROM client c ' \
+            f'WHERE c.client_id = {id};'
+
     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
         cursor.execute(query)
-        data = cursor.fetchall()
-    result = [dict(row) for row in data]
-    return jsonify(result)
+        data = cursor.fetchall()[0]
 
-@app.route('/inventory/<id>', methods=['GET'])
-def get_inventory_sales_prediction(id):
+    if data[0] == None:
+        customer_data = {'avg_monthly_spend': data[1], 'avg_lifespan': data[2]}
+        result = calculate_customer_lifetime_value(customer_data)
+        query = f'UPDATE client ' \
+                f'SET clv = {result} ' \
+                f'WHERE client_id = {id};'
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            cursor.execute(query)
+    else:
+        result = data[0]
+
+    return jsonify({'clv': result})
+
+
+
+
+#Pert Chart
+@app.route('/order/<id>/pert_chart', methods=['GET'])
+def get_pert_chart(id):
     conn = get_db_connection()
-    query = f'''SELECT product_id, sales_date, SUM(saled_quantity) AS total_sales
-                FROM sales
-                WHERE product_id = {id}
-                GROUP BY product_id, sales_date
-                ORDER BY product_id, sales_date ASC;'''
-    
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        cursor.execute(query)
-        data = cursor.fetchall()
-    result = [dict(row) for row in data]
-    return jsonify(result)
+    # query = f''
+
+    # with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+    #     cursor.execute(query)
+    #     data = cursor.fetchall()
 
 
-# product order progress
-@app.route('/product_order_progress', methods=['GET'])
-def get_product_orders():
-    conn = get_db_connection()
-    query = '''SELECT co.order_id, 
-                    ocp.product_id, 
-                    p.product_name, 
-                    m.machine_id, 
-                    co.created_at AS order_created_at, 
-                    co.delivered_at AS order_delivered_at
-                FROM client_order co
-                INNER JOIN order_contain_product ocp ON co.order_id = ocp.order_id
-                INNER JOIN product p ON ocp.product_id = p.product_id
-                INNER JOIN model m ON p.model_id = m.model_id;
-                '''
-    
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        cursor.execute(query)
-        data = cursor.fetchall()
-    result = [dict(row) for row in data]
-    return jsonify(result)
+    order_data = []
+    info = form_pert_chart_tree(order_data)
 
-@app.route('/product_order_progress/<product_type>', methods=['GET'])
-def get_certain_type_product_orders(product_type):
-    conn = get_db_connection()
-    print(product_type, type(product_type))
-    query = f'''SELECT 
-                    co.order_id, 
-                    ocp.product_id, 
-                    p.product_name, 
-                    m.machine_id, 
-                    co.created_at AS order_created_at, 
-                    co.delivered_at AS order_delivered_at
-                FROM 
-                    client_order co
-                INNER JOIN 
-                    order_contain_product ocp ON co.order_id = ocp.order_id
-                INNER JOIN 
-                    product p ON ocp.product_id = p.product_id
-                INNER JOIN 
-                    model m ON p.model_id = m.model_id
-                WHERE 
-                    p.product_type = '{product_type}';'''
-    
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        cursor.execute(query)
-        data = cursor.fetchall()
-    result = [dict(row) for row in data]
-    return jsonify(result)
-
-@app.route('/product_order_progress/<id>/<product_id>', methods=['GET'])
-def get_product_order_progress(id, product_id):
-    conn = get_db_connection()
-    query = f'''SELECT co.progress 
-                FROM client_order co 
-                INNER JOIN order_contain_product ocp ON co.order_id = ocp.order_id 
-                WHERE co.order_id = {id} AND ocp.product_id = {product_id};'''
-    
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        cursor.execute(query)
-        data = cursor.fetchall()
-    result = [dict(row) for row in data]
-    return jsonify(result)
+    return jsonify({'chart_info': info})
 
 
-# material order progress
-@app.route('/material_order_progress', methods=['GET'])
-def get_material_orders():
-    conn = get_db_connection()
-    query = '''SELECT 
-                    mo.material_order_id, 
-                    m.material_name, 
-                    ocm.quantity, 
-                    mo.created_at, 
-                    mo.delivered_at, 
-                    mo.progress
-               FROM 
-                    material_order mo
-               JOIN 
-                    order_contain_material ocm ON mo.material_order_id = ocm.material_order_id
-               JOIN 
-                    material m ON ocm.material_id = m.material_id;'''
-    
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        cursor.execute(query)
-        data = cursor.fetchall()
-    result = [dict(row) for row in data]
-    return jsonify(result)
-
-@app.route('/material_order_progress/<id>/<material_id>', methods=['GET'])
-def get_material_order_progress(id, material_id):
-    conn = get_db_connection()
-    query = f'''SELECT mo.progress 
-                FROM material_order mo
-                INNER JOIN order_contain_material ocm ON mo.material_order_id = ocm.material_order_id 
-                WHERE mo.material_order_id = {id} AND ocm.material_id = {material_id};'''
-    
-    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
-        cursor.execute(query)
-        data = cursor.fetchall()
-    result = [dict(row) for row in data]
-    return jsonify(result)
 
 if __name__ == '__main__':
-   app.run()
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8000)
--- utils.py
+++ utils.py
@@ -11,3 +11,84 @@ def calculate_growth_rates(sales_data):
             'growth_rate': growth_rate
         })
     return growth_rates
+
+def calculate_customer_lifetime_value(customer_data):
+    avg_monthly_spend = customer_data['avg_monthly_spend']
+    avg_lifespan = customer_data['avg_lifespan']
+    clv = avg_monthly_spend * avg_lifespan
+    return clv
+
+
+
+# ============================================================
+
+def form_pert_chart_tree(order_data):
+
+    info = {
+        "nodes": [
+            {
+                "no.": 1,
+                "name": "B1",
+                "quantity": 30,
+                "date": "2024-01-10"
+            },
+            {
+                "no.": 2,
+                "name": "B2",
+                "quantity": 90,
+                "date": "2024-01-10"
+            },
+            {
+                "no.": 3,
+                "name": "M_2",
+                "quantity": 60,
+                "date": "2023-12-27"
+            },
+            {
+                "no.": 4,
+                "name": "M_3",
+                "quantity": 120,
+                "date": "2023-12-29"
+            },
+            {
+                "no.": 5,
+                "name": "M_4",
+                "quantity": 100,
+                "date": "2024-01-04"
+            },
+            {
+                "no.": 6,
+                "name": "R_3",
+                "quantity": 120,
+                "date": "2023-12-20"
+            },
+            {
+                "no.": 7,
+                "name": "R_4",
+                "quantity": 480,
+                "date": "2024-12-23"
+            },
+            {
+                "no.": 8,
+                "name": "R_6",
+                "quantity": 120,
+                "date": "2024-12-23"
+            },
+            {
+                "no.": 9,
+                "name": "R_7",
+                "quantity": 300,
+                "date": "2024-12-29"
+            }
+        ]
+    }
+
+    return info
+
+def calculate_forcasting_data(sales_data):
+
+    return 0;
+
+def calculate_avg_purchase_time(customer_data):
+
+    return 0;
