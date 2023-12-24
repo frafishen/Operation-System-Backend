39adb49c0462271c9db0c8044d20ab234e1ab92f
frafishen <104518723+frafishen@users.noreply.github.com>
2023-12-24 20:47:29 +0800

    feat: add some apis for the p.11 on the slides

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask import (Flask, redirect, render_template, request,
 
 import psycopg2
 import psycopg2.extras
-from utils import calculate_growth_rates
+from utils import (calculate_growth_rates, calculate_customer_lifetime_value, form_pert_chart_tree,
+                    calculate_forcasting_data, calculate_average_purchase_time)
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -16,29 +18,6 @@ PASSWORD = 'mis'
 HOST = 'localhost'
 PORT = 8080
 
-
-@app.route('/')
-def index():
-   print('Request for index page received')
-   print("test Hello")
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
 def get_db_connection():
     conn = psycopg2.connect(database=DATABASE_NAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
     return conn
@@ -48,6 +27,11 @@ def handle_500(error):
     app.logger.error(f"500 error: {error}")  # Log the error
     return jsonify({"status": "failed", "message": "Internal server error"}), 500
 
+@app.route('/')
+def index():
+   print('Request for index page received')
+   print("test Hello")
+   return render_template('index.html')
 # p.10
 @app.route('/client', methods=['GET'])
 def get_client():
@@ -59,8 +43,8 @@ def get_client():
     result = [dict(row) for row in data]
     return jsonify(result)
 
-# p.11 (revenue)
-@app.route('/client/<id>', methods=['GET'])
+# p.11 (get growth rate)
+@app.route('/client/<id>/sgr', methods=['GET'])
 def get_company_sales(id):
     conn = get_db_connection()
     query = '''SELECT s.sales_date, SUM(p.price * s.saled_quantity) AS total_sales
@@ -68,16 +52,73 @@ def get_company_sales(id):
             JOIN product p ON s.product_id = p.product_id
             GROUP BY s.sales_date
             ORDER BY s.sales_date;'''
-    
+
     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
         cursor.execute(query)
         data = cursor.fetchall()
     result = [dict(row) for row in data]
     growth_rates = calculate_growth_rates(result)[-6:]
     result = result[-6:]
-    
+
     return jsonify({'sales revenue': result, 'growth rates': growth_rates})
 
+# unfinished
+@app.route('/client/<id>/clv', methods=['GET'])
+def get_customer_lifetime_value(id):
+    conn = get_db_connection()
+    query = f'SELECT c.clv, c.avg_monthly_spend, c.avg_lifespan ' \
+            f'FROM client c ' \
+            f'WHERE c.client_id = {id};'
+
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(query)
+        data = cursor.fetchall()[0]
+
+    if data[0] == None:
+        customer_data = {'avg_monthly_spend': data[1], 'avg_lifespan': data[2]}
+        result = calculate_customer_lifetime_value(customer_data)
+        query = f'UPDATE client ' \
+                f'SET clv = {result} ' \
+                f'WHERE client_id = {id};'
+
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            cursor.execute(query)
+    else:
+        result = data[0]
+
+    return jsonify({'clv': result})
+
+@app.route('/client/<id>/apt', methods=['GET'])
+def get_average_purchase_time(id):
+    conn = get_db_connection()
+    query = f'SELECT c.created_at ' \
+            f'FROM client_order c  ' \
+            f'WHERE client_id = {id};'
+
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+    result = [dict(row) for row in data]
+    apt = calculate_average_purchase_time(result)
+
+    return jsonify({'average purchase time': apt})
+
+#p.5 (Pert Chart)
+@app.route('/order/<id>/pert_chart', methods=['GET'])
+def get_pert_chart(id):
+    conn = get_db_connection()
+    # query = f''
+
+    # with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+    #     cursor.execute(query)
+    #     data = cursor.fetchall()
+
+
+    order_data = []
+    info = form_pert_chart_tree(order_data)
+
+    return jsonify({'chart_info': info})
 
 # p.6
 @app.route('/inventory', methods=['GET'])
--- utils.py
+++ utils.py
@@ -11,3 +11,103 @@ def calculate_growth_rates(sales_data):
             'growth_rate': growth_rate
         })
     return growth_rates
+
+def calculate_customer_lifetime_value(customer_data):
+    avg_monthly_spend = customer_data['avg_monthly_spend']
+    avg_lifespan = customer_data['avg_lifespan']
+    clv = avg_monthly_spend * avg_lifespan
+    return clv
+
+#moving average method
+def calculate_forcasting_data(inventory_data):
+    n = len(data)
+    window_size = 12
+
+    forecasted_data = []
+    for i in range(n - window_size + 1):
+        window = data[i:i + window_size]
+        average = sum(window) / window_size
+        forecasted_data.append(average)
+
+    return forecasted_data
+
+
+def calculate_average_purchase_time(purchase_dates):
+    n = len(purchase_dates)
+
+    total_time = 0
+    for i in range(1, n):
+        purchase_date_i = purchase_dates[i]['created_at']
+        purchase_date_prev = purchase_dates[i - 1]['created_at']
+
+        # 計算時間差
+        time_difference = purchase_date_i - purchase_date_prev
+        total_time += time_difference.days  # 假設以天為單位
+
+    # 計算平均購買時間
+    average_purchase_time = total_time / (n - 1)
+
+    return average_purchase_time
+
+def form_pert_chart_tree(order_data):
+
+    info = {
+        "nodes": [
+            {
+                "no.": 1,
+                "name": "B1",
+                "quantity": 30,
+                "date": "2024-01-10"
+            },
+            {
+                "no.": 2,
+                "name": "B2",
+                "quantity": 90,
+                "date": "2024-01-10"
+            },
+            {
+                "no.": 3,
+                "name": "M_2",
+                "quantity": 60,
+                "date": "2023-12-27"
+            },
+            {
+                "no.": 4,
+                "name": "M_3",
+                "quantity": 120,
+                "date": "2023-12-29"
+            },
+            {
+                "no.": 5,
+                "name": "M_4",
+                "quantity": 100,
+                "date": "2024-01-04"
+            },
+            {
+                "no.": 6,
+                "name": "R_3",
+                "quantity": 120,
+                "date": "2023-12-20"
+            },
+            {
+                "no.": 7,
+                "name": "R_4",
+                "quantity": 480,
+                "date": "2024-12-23"
+            },
+            {
+                "no.": 8,
+                "name": "R_6",
+                "quantity": 120,
+                "date": "2024-12-23"
+            },
+            {
+                "no.": 9,
+                "name": "R_7",
+                "quantity": 300,
+                "date": "2024-12-29"
+            }
+        ]
+    }
+
+    return info
